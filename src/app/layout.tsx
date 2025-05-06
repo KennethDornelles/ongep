28792f9f9dd96372fe133d8d6da417f6e08c7476
KennethDornelles <kennetholusegun6@gmail.com>
2025-05-06 10:07:56 -0300

    fix: :ambulance: importing and configuring Tailwind's global CSS to ensure consistent styling across browsers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import { Inter } from "next/font/google"
-import "./globals.css"
+import "../styles/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import Header from "@/components/header"
+import type { ReactNode } from "react"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -9,14 +10,14 @@ export const metadata = {
   title: "ONGEP - Organização Não Governamental para Educação e Projetos Sociais",
   description:
     "Promovendo educação, cultura e desenvolvimento social através de projetos que transformam vidas e comunidades.",
-    generator: 'v0.dev'
+  generator: 'v0.dev'
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light">
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
           <Header />
           {children}
         </ThemeProvider>
